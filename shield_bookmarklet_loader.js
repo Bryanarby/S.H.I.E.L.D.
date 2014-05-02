8c53749583240f98ff92dbe2d4d9713e6eace8ce
Bryanarby <just.me.Bry@gmail.com>
2014-05-02 19:58:56 +0200

    Create shield_bookmarklet_loader.js

---
+++ shield_bookmarklet_loader.js
@@ -0,0 +1,9 @@
+javascript:(
+  function () {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('id', 'shieldScript');
+    js.setAttribute('src', 'https://raw.github.com/Bryanarby/S.H.I.E.D./master/shield.js');
+    document.head.appendChild(js);
+ }()
+);
