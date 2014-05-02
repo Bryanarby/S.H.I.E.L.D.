757a7cc6c94222d5ad3c18c0f1efb6086fcd12f1
Bryanarby <just.me.Bry@gmail.com>
2014-05-02 19:33:37 +0200

    Create shield.js

---
+++ shield.js
@@ -0,0 +1,52 @@
+// Global Variables
+var scriptElement = document.getElementById( 'shieldScript' ),
+	baseUrl = scriptElement !== null ?
+		scriptElement.getAttribute('src').replace(/\/schieldScript\.js$/, '') :
+		'https://raw.github.com/Bryanarby/S.H.I.E.L.D./master',
+	SHIELD = {
+		'baseUrl': baseUrl,
+		'branch' : 'M',
+		'version': 0.1b1
+	};
+
+// Load external libraries
+var script_list = [
+    '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js',
+    '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css',
+    '//cdn.jsdelivr.net/underscorejs/1.6.0/underscore-min.js',
+    SHIELD.baseUrl + '/shield_main.js',
+  ]
+  
+SHIELD.loadInterval = setInterval(function() {
+  if (game) {
+    clearInterval(SHIELD.loadInterval);
+    SHIELD.loadInterval = 0;
+    shieldInit();
+  }
+}, 1000);
+
+function loadScript(id) {
+  if (id >= script_list.length) {
+    setOverrides();
+    SHIELDStart();
+  } else { 
+    var url = script_list[id];
+    if (/\.js$/.exec(url)) {
+      $.getScript(url, function() {loadScript(id + 1);});
+    } else if (/\.css$/.exec(url)) {
+      $('<link>').attr({rel: 'stylesheet', type: 'text/css', href: url}).appendTo($('head'));
+      loadScript(id + 1);
+    } else {
+      console.log('Error loading script: ' + url);
+      loadScript(id + 1);
+    }
+  }
+}
+
+function shieldInit() {
+  var jquery = document.createElement('script');
+  jquery.setAttribute('type', 'text/javascript');
+  jquery.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+  jquery.onload = function() {loadScript(0);};
+  document.head.appendChild(jquery);
+}
