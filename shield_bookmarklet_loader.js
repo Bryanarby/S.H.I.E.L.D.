3e3a77c6877323b46d8a43fd44da40af6734d770
Bryanarby <just.me.Bry@gmail.com>
2014-05-02 20:00:17 +0200

    Update shield_bookmarklet_loader.js

--- shield_bookmarklet_loader.js
+++ shield_bookmarklet_loader.js
@@ -3,7 +3,7 @@ javascript:(
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
     js.setAttribute('id', 'shieldScript');
-    js.setAttribute('src', 'https://raw.github.com/Bryanarby/S.H.I.E.D./master/shield.js');
+    js.setAttribute('src', 'https://raw.github.com/Bryanarby/S.H.I.E.L.D./master/shield.js');
     document.head.appendChild(js);
  }()
 );
